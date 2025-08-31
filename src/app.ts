3317ef2f300de9737968d66d72465d8fea02066d
Fátima Kraiczyi <fatimakraiczyi@gmail.com>
2025-08-31 00:46:04 -0300

    adiciona suporte ao Swagger UI

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from 'express';
 import livroRoutes from './routes/livroRoutes';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
 
 const app = express();
 
 app.use(express.json());
 app.use('/livros', livroRoutes);
+
+// Swagger UI
+const swaggerFile = path.resolve(__dirname, '../swagger.yaml');
+const swaggerDocument = yaml.load(fs.readFileSync(swaggerFile, 'utf8')) as object;
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({
